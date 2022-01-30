26773a18f7b421f5559efb865658be43111e2e85
asad23raza <asad18raza@gmail.com>
2022-01-30 01:12:10 -0500

    commit on master

--- src/components/Matrixcontainer.js
+++ src/components/Matrixcontainer.js
@@ -1,8 +1,64 @@
 import React from "react";
+var matrix_1 = {
+    vectors: [
+        {
+            x1: 5,
+            x2: 8,
+            x3: 11
+        },
+        {
+            x1: 6,
+            x2: 9,
+            x3: 1
+        },
+        {
+            x1: 7,
+            x2: 10,
+            x3: 12
+        }
+    ]
+}
 class MatrixContainer extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Setup development environment",
+                checked: false
+            },
+            {
+                id: 2,
+                title: "Develop website add content",
+                checked: false
+            }
+        ]
+    };
     render() {
         return (
-                <h1>Hello World</h1>
+            <div>
+            <ul>
+                {this.state.todos.map(elem => (
+                    <li>{elem.title}</li>
+                ))}
+            </ul>
+            <table>
+                <tr>
+                    {matrix_1.vectors.map(elem => (
+                        <td>{elem.x1}</td>
+                    ))}
+                </tr>
+                <tr>
+                    {matrix_1.vectors.map(elem => (
+                        <td>{elem.x2}</td>
+                    ))}
+                </tr>
+                <tr>
+                    {matrix_1.vectors.map(elem => (
+                        <td>{elem.x3}</td>
+                    ))}
+                </tr>
+            </table>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom";
 //const title = <h1>Enter the Matrix you Would Like to Reduce Below</h1>
-import MatrixContainer from "./components/Matrixcontainer";
-ReactDom.render(<MatrixContainer />, document.getElementById("root"));
+import MatrixContainer from "./components/MatrixContainer";
+ReactDom.render(
+  <MatrixContainer />, 
+  document.getElementById("root")
+);
 
