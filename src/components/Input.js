7c57c99bbb0b5eceac7c940aa07a17dbca2b61bc
asad23raza <asad18raza@gmail.com>
2022-01-30 01:11:54 -0500

    added matrices

---
+++ src/components/Input.js
@@ -0,0 +1,8 @@
+import React from "react";
+class InputNode extends React.Component {
+    state = {
+        matrices: [],
+        
+    }
+}
+export default InputNode
---
+++ src/components/MatrixList.js
@@ -0,0 +1,8 @@
+import React from "react";
+class MatrixList extends React.Component {
+    state = {
+        matrices: [],
+        
+    }
+}
+export default MatrixList
