d5d7a0b2649b3c273dde07efab469cf57bb436f4
asad23raza <asad18raza@gmail.com>
2022-02-19 00:01:02 -0500

    2022/02/19

--- src/components/MatrixList.js
+++ src/components/MatrixList.js
@@ -1,8 +1,33 @@
 import React from "react";
 class MatrixList extends React.Component {
-    state = {
-        matrices: [],
-        
+    render() {
+        return (
+            <div>
+            {this.props.matrix_list.map(
+            elem => (
+                    <div key={elem.id}>
+                        <table>
+                            <tbody>
+                            {elem.matrix.map(
+                                row => (
+                                    <tr key={row.id}>
+                                        {row.map(
+                                            subentry => (
+                                                <td key={subentry.id}>
+                                                    {subentry}
+                                                </td>
+                                            )
+                                        )}
+                                    </tr>
+                                )
+                            )}
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            )}
+        </div>
+        )
     }
 }
 export default MatrixList
--- src/components/Matrixcontainer.js
+++ src/components/Matrixcontainer.js
@@ -5,62 +5,46 @@ class MatrixContainer extends React.Component {
         matrix_list: [
             {
                 id: 1,
-                matrix: [[1,0,0],[0,1,0],[0,0,1]],
-                checked: false
+                matrix: [[1,3,1],[0,1,0],[0,0,1]],
             },
             {
                 id: 2,
                 matrix: [[4,5,6],[4,5,6],[4,5,6]],
-                checked: false
+            },
+            {
+                id: 3,
+                matrix: [[0,0,1],[0,1,0],[1,0,0]],
             }
         ]
     };
     render() {
-        function sqrt(x1) {
-            return x1 * x1
-        }
         return (
             <div>
-                {this.MatricesDict.matrix_list.map(
-                    elem => (
-                        <table>
-                            <tr>
-                                <td>
-                                {elem.matrix[0][0]}
-                                </td>
-                                <td>
-                                {elem.matrix[1][0]}
-                                </td>
-                                <td>
-                                {elem.matrix[2][0]}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                {elem.matrix[0][1]}
-                                </td>
-                                <td>
-                                {elem.matrix[1][1]}
-                                </td>
-                                <td>
-                                {elem.matrix[2][1]}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                {elem.matrix[0][2]}
-                                </td>
-                                <td>
-                                {elem.matrix[1][2]}
-                                </td>
-                                <td>
-                                {elem.matrix[2][2]}
-                                </td>
-                            </tr>
-                        </table>
-                    )
-                )}
+                <MatrixList matrix_list={this.MatricesDict.matrix_list} />
             </div>
+            // <div>
+            //     {this.MatricesDict.matrix_list.map(
+            //     elem => (
+            //             <div>
+            //                 <table>
+            //                     {elem.matrix.map(
+            //                         row => (
+            //                             <tr>
+            //                                 {row.map(
+            //                                     subentry => (
+            //                                         <td>
+            //                                             {subentry}
+            //                                         </td>
+            //                                     )
+            //                                 )}
+            //                             </tr>
+            //                         )
+            //                     )}
+            //                 </table>
+            //             </div>
+            //         )
+            //     )}
+            // </div>
         )
     }
 }
