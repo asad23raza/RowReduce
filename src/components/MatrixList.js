2746e85c668bc3f40dcb10c7691b082bd51c7959
asad23raza <asad18raza@gmail.com>
2022-02-19 17:10:51 -0500

    functional

--- src/components/MatrixList.js
+++ src/components/MatrixList.js
@@ -1,19 +1,33 @@
 import React from "react";
-import InputNode from "./Input";
 class MatrixList extends React.Component {
     render() {
         return (
             <div>
             {this.props.matrix_list.map(
             elem => (
-                <div key={elem.id}>
-                    {/* matrix_list_elem should not be confused with matrix(list of numbers), or matrix_list(list of matrix dictionaries) */}
-                    <InputNode key={elem.id} matrix_list_elem={elem} />
-                </div>
+                    <div key={elem.id}>
+                        <table>
+                            <tbody>
+                            {elem.matrix.map(
+                                row => (
+                                    <tr key={row.id}>
+                                        {row.map(
+                                            subentry => (
+                                                <td key={subentry.id}>
+                                                    {subentry}
+                                                </td>
+                                            )
+                                        )}
+                                    </tr>
+                                )
+                            )}
+                            </tbody>
+                        </table>
+                    </div>
                 )
             )}
         </div>
         )
-                                            }
+    }
 }
 export default MatrixList
