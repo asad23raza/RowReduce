f069496b399bd587c6f8508730851a7bd8194ecc
asad23raza <asad18raza@gmail.com>
2022-02-20 17:14:46 -0500

    working

--- src/components/EnterMatrix.js
+++ src/components/EnterMatrix.js
@@ -1,9 +1,15 @@
 import React from "react";
 class TextField extends React.Component {
+    state = {
+        matrix: ""
+    }
     render () {
         return (
         <div>
+            <span>
             <input type="text" placeholder="Add Matrix" />
+            <button>Row Reduce</button>
+            </span>
         </div>
         )
     }
