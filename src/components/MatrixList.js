956bf06b5bc579dd5cce271b9e42d54cba37eb30
asad23raza <asad18raza@gmail.com>
2022-02-20 11:32:33 -0500

    fixed bug in input.js (in react <nextfile a={b}> can be called as this.props.a not this.props.b

--- src/components/MatrixList.js
+++ src/components/MatrixList.js
@@ -1,32 +1,42 @@
 import React from "react";
+import InputNode from "./Input";
 class MatrixList extends React.Component {
     render() {
         return (
             <div>
             {this.props.matrix_list.map(
-            elem => (
+                elem => (
                     <div key={elem.id}>
-                        <table>
-                            <tbody>
-                            {elem.matrix.map(
-                                row => (
-                                    <tr key={row.id}>
-                                        {row.map(
-                                            subentry => (
-                                                <td key={subentry.id}>
-                                                    {subentry}
-                                                </td>
-                                            )
-                                        )}
-                                    </tr>
-                                )
-                            )}
-                            </tbody>
-                        </table>
+                        <InputNode matrix_list_elem={elem} />
                     </div>
                 )
             )}
-        </div>
+            </div>
+    //    <div>
+    //         {this.props.matrix_list.map(
+    //         elem => (
+    //                 <div key={elem.id}>
+    //                     <table>
+    //                         <tbody>
+    //                         {elem.matrix.map(
+    //                             row => (
+    //                                 <tr key={row.id}>
+    //                                     {row.map(
+    //                                         subentry => (
+    //                                             <td key={subentry.id}>
+    //                                                 {subentry}
+    //                                             </td>
+    //                                         )
+    //                                     )}
+    //                                 </tr>
+    //                             )
+    //                         )}
+    //                         </tbody>
+    //                     </table>
+    //                 </div>
+    //         )
+    //         )}
+    //     </div>     
         )
     }
 }
