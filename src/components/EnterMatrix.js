058ef8323aabc06accf9200c73d10ce23664a855
asad23raza <asad18raza@gmail.com>
2022-02-21 22:26:52 -0500

    inputs recieved in console.log

--- src/components/EnterMatrix.js
+++ src/components/EnterMatrix.js
@@ -3,15 +3,34 @@ class TextField extends React.Component {
     state = {
         matrix: ""
     }
+    onChange = (e) => {
+        this.setState({
+            matrix: e.target.value
+        });
+    };
+    // EntryFilter = (e) => {
+    //     let val = e.charCode
+    //     if ((val >= 48 && val <= 57) ||
+    //         (val === 93 || val === 91 || val === 44)) {
+    //         console.log("acceptable number")
+    //         return true
+    //     }
+    //     else {
+    //         console.log("not acceptable")
+    //         return false
+    //     }
+    // }
     render () {
         return (
         <div>
             <span>
-            <input type="text" placeholder="Add Matrix" />
-            <button>Row Reduce</button>
+            <input id="InputText" type="text" placeholder="Add Matrix" value={this.state.matrix}
+            onChange={this.onChange} /*onKeyPress={this.EntryFilter}*/ />
+            <button onClick={() => this.props.add_function()}>Row Reduce</button>
             </span>
         </div>
         )
+        
     }
 }
 export default TextField
