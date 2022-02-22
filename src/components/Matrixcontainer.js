ffc7b965a65f47c57278ed2bd10af9bdda830040
asad23raza <asad18raza@gmail.com>
2022-02-21 22:26:38 -0500

    inputs recieved

--- src/components/Matrixcontainer.js
+++ src/components/Matrixcontainer.js
@@ -29,11 +29,42 @@ class MatrixContainer extends React.Component {
         })
         console.log("afterdeletion" + this.MatricesDict.matrix_list.length)
     }
+    CheckArr = (arr) => {
+
+    }
+    AddMatrix = () => {
+        //console.log("add button pressed")
+        let InputText = document.getElementById("InputText").value
+        //console.log(InputText)
+        let retarr = []
+        let length = InputText.length
+        let acceptable_chars = ["0","1","2","3","4","5","6","7","8","9", "]","[",","]
+        let numbers = acceptable_chars.slice(0,-3)
+        for (var i = 0; i <= length; i++) {
+            if(InputText[i] in numbers) {
+                retarr.push(parseInt(InputText[i]));
+            }
+        }
+        let retarr_length = retarr.length
+        let vectors = 3
+        let dimension = 3
+        let newarr = []
+        for (var i = 0; i < vectors; ++i) {
+            let subarr = []
+            for (var j = i; j < retarr_length; j += dimension) {
+                subarr.push(retarr[j])
+            }
+            newarr.push(subarr)
+        }
+        console.log("input text :" +
+        InputText,"length: "+length, retarr, newarr, retarr_length)
+    }
+    
     render() {
         return (
             <div>
                 <Header />
-                <TextField />
+                <TextField add_function={this.AddMatrix} />
                 <MatrixList matrix_list={this.MatricesDict.matrix_list}
                 DelMatrix = {this.DelMatrix} />
             </div>
@@ -41,3 +72,4 @@ class MatrixContainer extends React.Component {
     }
 }
 export default MatrixContainer
+
